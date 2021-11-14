7954ef8b683b1437c5a7f4bc6594de2f8f019436
Yakubov01 <lord17cl@gmail.com>
2021-11-14 15:00:05 +0500

    change Main

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,3 +1,22 @@
-export default function Main() {
-  return <div className="container content"></div>;
+import React from "react";
+import Movies from "../components/Movies";
+
+export default class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=329ffa13&s=panda")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    return (
+      <div className="container content">
+        <Movies movies={this.state.movies} />
+      </div>
+    );
+  }
 }
